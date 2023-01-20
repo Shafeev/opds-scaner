bc9c5626520800903dafd7f66e7dbbd4510a0572
Makhmud Shafeev <shafeevmkf@gmail.com>
2023-01-21 00:23:02 +0300

    add ForeignKey
    (cherry picked from commit a522097f5978ab60c3468a97abeb73bb5cd1d042)

--- src/main/java/mcs/opds/scan/RunApp.java
+++ src/main/java/mcs/opds/scan/RunApp.java
@@ -1,9 +1,6 @@
 package mcs.opds.scan;
 
-import mcs.opds.scan.models.Author;
-import mcs.opds.scan.models.Book;
-import mcs.opds.scan.models.Catalog;
-import mcs.opds.scan.models.Genre;
+import mcs.opds.scan.models.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
